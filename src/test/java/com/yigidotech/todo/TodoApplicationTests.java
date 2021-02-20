1cd30014b89a5c7fbe71619223c3d93adbc3c642
Korkmaz <hakan.korkmaz@iqb.solutions>
2021-02-20 16:42:08 +0300

    initialize

---
+++ src/test/java/com/yigidotech/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yigidotech.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
